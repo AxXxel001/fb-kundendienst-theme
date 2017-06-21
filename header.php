6026b3ef99ef7221a3f990bf413cb8db3872e0a8
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-06-21 18:58:52 +0200

    Update

--- header.php
+++ header.php
@@ -12,6 +12,7 @@ $template_root = get_template_directory_uri();
     <link href="<?php echo $template_root;?>/css/normalize.css" rel="stylesheet" type="text/css">
     <link href="<?php echo $template_root;?>/css/webflow.css" rel="stylesheet" type="text/css">
     <link href="<?php echo $template_root;?>/css/fb-kundendienst-theme.webflow.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $template_root;?>/css/custom_css.css" rel="stylesheet" type="text/css">
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">
         !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
