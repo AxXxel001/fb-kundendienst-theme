890112ab18663415417eee49dc1115b88c345ceb
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-10-15 21:19:27 +0200

    Update

--- single.php
+++ single.php
@@ -76,7 +76,7 @@ Template Name: Single Blogpost
 
                             <?php if( get_row_layout() == 'kundendienst-kommentar' ): ?>
 
-                                <div class="fb-comment-image-wrapper"><img src="<?php echo get_theme_root_uri()+'/images/fb-kundendienst.jpeg'; ?>" class="fb-comment-image"></div>
+                                <div class="fb-comment-image-wrapper"><img src="<?php echo get_theme_root_uri() . '/images/fb-kundendienst.jpeg'; ?>" class="fb-comment-image"></div>
                                 <div class="fb-comment-data-wrapper">
                                     <div><span class="fb-comment-name"><span>Kundendienst</span></span><?php the_sub_field('text'); ?></div>
                                 </div>
